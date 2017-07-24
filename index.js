56dd038aba5fc860c154eee7ec74fff21d535e50
David Street <david@exclamationlabs.com>
2017-07-24 12:54:00 -0400

    Export client service class

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
 	Server: require('./lib/server'),
 	Service: require('./lib/service'),
-	Client: require('./lib/client')
+	Client: require('./lib/client'),
+	ClientService: require('./lib/client-service')
 }
